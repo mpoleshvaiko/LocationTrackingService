103470b9357481b0607cc1c6bc3a265f44b423b8
mariia.poleshvaiko@gmail.com <mariia.poleshvaiko@gmail.com>
2023-07-05 21:53:26 +0200

    map view added

--- app/src/main/java/com/example/locationtrackingservice/MainActivity.kt
+++ app/src/main/java/com/example/locationtrackingservice/MainActivity.kt
@@ -1,11 +1,138 @@
 package com.example.locationtrackingservice
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import com.example.locationtrackingservice.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mapView: MapView
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        getLastLocationAndCheckPermissions()
+    }
+
+    private fun getLastLocationAndCheckPermissions() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    mapView.getMapAsync { maps ->
+                        val latLng = LatLng(location.latitude, location.longitude)
+                        val markerOptions =
+                            MarkerOptions().position(latLng).title("Current Location")
+
+                        maps.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+                        maps.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+                        maps.addMarker(markerOptions)
+                    }
+                }
+            }
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission() {
+        if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) &&
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)
+        ) {
+            binding.root.showSnackbar(
+                "Location access required",
+                Snackbar.LENGTH_INDEFINITE,
+                "OK"
+            ) {
+                launchMultiplePermission()
+            }
+        } else {
+            launchMultiplePermission()
+        }
+    }
+
+    private fun launchMultiplePermission() =
+        requestMultiplePermissions
+            .launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+
+    private val requestMultiplePermissions =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                Log.d("DEBUG", "${it.key} = ${it.value}")
+            }
+            if (permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, true)
+                && permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, true)
+            ) {
+                getLastLocationAndCheckPermissions()
+            }
+        }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
---
+++ app/src/main/java/com/example/locationtrackingservice/Utils.kt
@@ -0,0 +1,18 @@
+package com.example.locationtrackingservice
+
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+
+fun View.showSnackbar(
+    msg: String,
+    length: Int,
+    actionMessage: CharSequence?,
+    action: (View) -> Unit
+) {
+    val snackbar = Snackbar.make(this, msg, length)
+    if (actionMessage != null) {
+        snackbar.setAction(actionMessage) {
+            action(this)
+        }.show()
+    }
+}
