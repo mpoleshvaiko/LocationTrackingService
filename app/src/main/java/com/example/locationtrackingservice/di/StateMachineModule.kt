daa40afaef2c80385fad80e2e67ae51c01fc72e4
mariia.poleshvaiko@gmail.com <mariia.poleshvaiko@gmail.com>
2023-08-15 18:09:50 +0200

    - added foreground service to keep saving locations to database even when the application is in the foreground - moved saving logic to foreground service

--- app/src/main/java/com/example/locationtrackingservice/di/StateMachineModule.kt
+++ app/src/main/java/com/example/locationtrackingservice/di/StateMachineModule.kt
@@ -2,8 +2,14 @@ package com.example.locationtrackingservice.di
 
 import com.example.locationtrackingservice.stateMachine.LocationTrackingStateMachine
 import com.example.locationtrackingservice.stateMachine.LocationTrackingStateMachineImpl
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val stateMachine = module {
-    single<LocationTrackingStateMachine> { LocationTrackingStateMachineImpl(get(), get()) }
+    single<LocationTrackingStateMachine> {
+        LocationTrackingStateMachineImpl(
+            get(),
+            androidContext()
+        )
+    }
 }
--- app/src/main/java/com/example/locationtrackingservice/stateMachine/LocationTrackingStateMachineImpl.kt
+++ app/src/main/java/com/example/locationtrackingservice/stateMachine/LocationTrackingStateMachineImpl.kt
@@ -1,29 +1,23 @@
 package com.example.locationtrackingservice.stateMachine
 
+import android.content.Context
 import android.location.Location
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.locationtrackingservice.LOG_TAG_STATE
-import com.example.locationtrackingservice.database.LocationEntity
 import com.example.locationtrackingservice.managers.location.LocationManager
-import com.example.locationtrackingservice.repository.LocationRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancelChildren
+import com.example.locationtrackingservice.service.LocationForegroundService
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 
 
 class LocationTrackingStateMachineImpl(
-    private val locationRepository: LocationRepository,
-    private val locationManager: LocationManager
+    private val locationManager: LocationManager,
+    private val applicationContext: Context
 ) :
     LocationTrackingStateMachine {
     private val _currentState = MutableLiveData<States>(States.IDLE)
     override val currentState: LiveData<States> get() = _currentState
-    private val scope = CoroutineScope(Dispatchers.Main)
 
     override fun transitionTo(newState: States) {
         handleStateTransition(newState)
@@ -32,11 +26,6 @@ class LocationTrackingStateMachineImpl(
     override fun getLocationUpdates(): Flow<Location?> = locationManager.requestLocationUpdate()
     override fun removeLocationUpdates() {
         locationManager.removeLocationUpdate()
-    }
-
-    private suspend fun saveLocationToDatabase(location: Location) {
-        val locationEntity = LocationEntity(location = "$location")
-        locationRepository.insertLocation(locationEntity)
     }
 
     private fun handleStateTransition(newState: States) {
@@ -49,20 +38,17 @@ class LocationTrackingStateMachineImpl(
             }
             States.RUNNING -> {
                 Log.d(LOG_TAG_STATE, "RUNNING")
-                getLocationUpdates()
-                    .onEach {
-                        if (it != null) {
-                            saveLocationToDatabase(it)
-                        }
-                    }
-                    .launchIn(scope)
+                LocationForegroundService.startService(
+                    applicationContext,
+                    "Foreground Service Running"
+                )
             }
             States.PAUSE -> {
                 Log.d(LOG_TAG_STATE, "PAUSE")
             }
             States.DONE -> {
                 Log.d(LOG_TAG_STATE, "DONE")
-                cancel()
+                LocationForegroundService.stopService(applicationContext)
             }
             States.ERROR -> {
                 Log.e(LOG_TAG_STATE, "ERROR")
@@ -70,6 +56,4 @@ class LocationTrackingStateMachineImpl(
         }
         _currentState.value = newState
     }
-
-    private fun cancel() = scope.coroutineContext.cancelChildren()
 }
